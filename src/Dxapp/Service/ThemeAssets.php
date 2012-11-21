8802fde7932d21d18b5f72e7ab989603523e5709
Dennes B Abing <dennes.b.abing@gmail.com>
2012-11-22 00:25:48 +0800

    Fix not found class: FileSystemCache

--- src/Dxapp/Service/ThemeAssets.php
+++ src/Dxapp/Service/ThemeAssets.php
@@ -5,7 +5,12 @@ namespace Dxapp\Service;
 use AsseticBundle\Service as AsseticService,
 	Assetic\Factory,
 	Assetic\Asset\AssetInterface,
-	Assetic\AssetWriter;
+	Assetic\AssetWriter,
+    Assetic\Factory,
+    Assetic\AssetWriter,
+    Assetic\Asset\AssetInterface,
+    Assetic\Asset\AssetCache,
+    Assetic\Cache\FilesystemCache;
 
 class ThemeAssets extends AsseticService
 {
